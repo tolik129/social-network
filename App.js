377196c16613d26cc4996a1b1f55b0632c71580c
tolik129 <boykoanatoliy@ukr.net>
2021-10-04 19:48:11 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -24,7 +24,11 @@ const App = (props) => {
          <Route path='/dialogs' 
          render={ () => <Dialogs state={props.state.dialogsPage} />} />
          <Route path='/profile' 
-         render={ () => <Profile state={props.state.profilePage} />} />
+         render={ () => <Profile 
+          profilePage={props.state.profilePage} 
+          addPost={props.addPost} 
+          updateNewPostText={props.updateNewPostText}
+          />} />
         </div>
       </div>
       </BrowserRouter>
--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import state, {subscribe} from "./redux/state";
 import './index.css';
 import App from './App';
+import {addPost, updateNewPostText} from './redux/state';
+import {BrowserRouter} from "react-router-dom";
+
 import reportWebVitals from './reportWebVitals';
-import state from './redux/state';
 
 
 
 
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App state={state} />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+
+let rerenderEntireTree = () => {
+  ReactDOM.render(
+    <BrowserRouter>
+       <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+    </BrowserRouter>, document.getElementById('root'));
+}
+
+
+
+rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
